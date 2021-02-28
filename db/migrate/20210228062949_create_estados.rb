02cb8c5c32fa5eef0def809ac6495be7751f214e
Barbara Carrijo <barbarahcarrijo@gmail.com>
2021-02-28 02:47:08 -0400

    adiciona estrutura de estado

---
+++ db/migrate/20210228062949_create_estados.rb
@@ -0,0 +1,10 @@
+class CreateEstados < ActiveRecord::Migration[5.2]
+  def change
+    create_table :estados do |t|
+      t.string :sgUf
+      t.text :nome
+
+      t.timestamps
+    end
+  end
+end
